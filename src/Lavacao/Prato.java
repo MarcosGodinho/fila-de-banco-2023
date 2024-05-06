8430867c8964f2d224cd5deb9d62b66769526cc0
Marcos Godinho <marcosenrico@hotmail.com>
2024-05-06 19:55:38 -0300

    First commit

---
+++ src/Lavacao/Prato.java
@@ -0,0 +1,22 @@
+package Lavacao;
+
+public class Prato {
+    private int id;
+
+    public Prato(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Prato " + id;
+    }
+}
